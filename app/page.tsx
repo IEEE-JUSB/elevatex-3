9b870372ad7c5f282a908fa7e360718169ff56e1
Sagnik Goswami <sagnik.gos06@gmail.com>
2025-10-24 04:34:25 +0530

    added AboutSection

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import Sponsors from "../components/sponsor";
 import Speakers from "../components/speakers/speakers";
 import Schedule from "../components/Schedule";
+import AboutSection from "../components/AboutSection";
 
 export default async function Home() {
   return (
     <div>
+      <AboutSection />
       <Sponsors />
       <Speakers />
       <Schedule />
