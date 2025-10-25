e98651d0ad4c1f4daabedc50e498fdb87991b0d4
rahul-p19 <pandeyrahul.work@gmail.com>
2025-10-25 12:30:50 +0530

    init backend

---
+++ prisma.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig, env } from "prisma/config";
+
+process.loadEnvFile();
+
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+  migrations: {
+    path: "prisma/migrations",
+  },
+  engine: "classic",
+  datasource: {
+    url: env("DATABASE_URL"),
+  },
+});
