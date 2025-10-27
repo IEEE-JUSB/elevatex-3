f5f691db97c11aacfa10adf61a452d57fe965f00
rahul-p19 <pandeyrahul.work@gmail.com>
2025-10-27 19:21:28 +0530

    mail fix attempt

--- lib/mail.ts
+++ lib/mail.ts
@@ -1,9 +1,10 @@
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST,
-    port: parseInt(process.env.SMTP_PORT || "587"),
-    secure: process.env.SMTP_SECURE === "false",
+    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
