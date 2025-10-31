fceda9df16ceddc80c8eac455195aab465bb5830
PiyushBose <piyushbose05@gmail.com>
2025-11-01 02:11:45 +0530

    Carousel added

---
+++ components/Carousel.tsx
@@ -0,0 +1,52 @@
+// Carousel.tsx
+"use client"
+
+import * as React from "react"
+import Autoplay from "embla-carousel-autoplay"
+
+import { Card, CardContent } from "@/components/ui/card"
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+import Image from "next/image"
+
+export function CarouselPlugin() {
+  const plugin = React.useRef(
+    Autoplay({ delay: 2000, stopOnInteraction: true })
+  )
+
+  return (
+    <Carousel
+      plugins={[plugin.current]}
+      className="w-full"
+      onMouseEnter={plugin.current.stop}
+      onMouseLeave={plugin.current.reset}
+    >
+      <CarouselContent>
+        {Array.from({ length: 5 }).map((_, index) => (
+          <CarouselItem key={index}>
+            <div>
+              <Card>
+                <CardContent>
+                  <Image
+                    src="/elevatex-logo-black.png"
+                    alt="Carousel Slides"
+                    width={400}
+                    height={500}
+                    className="w-full h-auto object-cover rounded-2xl shadow-sm"
+                /> 
+                </CardContent>
+              </Card>
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+  )
+}
